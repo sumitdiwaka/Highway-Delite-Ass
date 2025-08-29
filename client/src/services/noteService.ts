8416c41e3e1c0938e29ca1f6dba188a85f6a30c4
unknown <sumitdiwakar476@gmai.com>
2025-08-29 20:47:19 +0530

    Url Updated

--- client/src/services/noteService.ts
+++ client/src/services/noteService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/notes';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
 // Define the shape of a Note object
 export interface INote {
